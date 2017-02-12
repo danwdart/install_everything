a40e8d5e9a83431b45d6ea4011b6bedb42d415c3
Dan <git@dandart.co.uk>
2017-02-12 18:10:10 +0000

    Install everything script

---
+++ install-everything.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+add-apt-repository -y ppa:bitcoin/bitcoin
+add-apt-repository -y ppa:dansmith/chirp-snapshots
+add-apt-repository -y ppa:kamalmostafa/fldigi
+add-apt-repository -y ppa:kubuntu-ppa/backports
+add-apt-repository -y ppa:oibaf/graphics-drivers
+add-apt-repository -y ppa:ondrej/php
+add-apt-repository -y ppa:ubuntu-hams-updates
+add-apt-repository -y ppa:webupd8team/atom
+add-apt-repository -y ppa:wine/wine-builds
+
+apt install
